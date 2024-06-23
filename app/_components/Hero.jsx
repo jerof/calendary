05f275cb61874af35d5c7d8873500fa1c62fbb5c
Nikhil Samuel <nikhilsamuel.89@gmail.com>
2024-06-23 21:37:19 +0200

    Hero completed

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -1,7 +1,31 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import React from "react";
 
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <div className="grid grid-cols-2 mt-10">
+      <div className="flex flex-col pl-24 pr-4 space-y-10 mt-10">
+        <h1 className="text-6xl font-bold">Easy scheduling ahead</h1>
+        <p className="text-xl">
+          Calendary is your scheduling automation platform for eliminating the
+          back-and-forth emails to find the perfect time — and so much more.
+        </p>
+        <div>
+          <Button className="px-10 py-6 text-lg">Sign up with Google</Button>
+        </div>
+      </div>
+      <div className="flex justify-center items-center pr-10">
+        <Image
+          src="/heroImage.png"
+          width={600}
+          height={500}
+          alt="hero image"
+          className="object-cover"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
